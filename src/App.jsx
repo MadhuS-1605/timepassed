0cb46eae316411b9d94bc08845263b0d0b98b8ad
MadhuS-1605 <madhusgowda56@gmail.com>
2026-01-04 21:28:37 +0530

    added vercel analytics

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Compare from './pages/Compare';
 import './index.css';
+import { Analytics } from "@vercel/analytics/next"
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/compare" element={<Compare />} />
       </Routes>
+      <Analytics />
     </Router>
   );
 }
